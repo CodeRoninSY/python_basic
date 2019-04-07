052adf17550d47b9d8025a2db313ff1e0cec998f
CodeRoninSY <45330933+CodeRoninSY@users.noreply.github.com>
2019-04-07 15:06:02 +0300

    Add files via upload

--- decorators.py
+++ decorators.py
@@ -6,7 +6,7 @@ Decorators example
 
 '''
 
-from decorlib import decorator, timer
+from decorlib import decorator, timer, debug
 
 #  # Decorators
 #  def decor(func):
@@ -59,16 +59,18 @@ from decorlib import decorator, timer
 #  selam("Mondo")
 
 
+# @decorator
 @decorator
 def r_selam(nome):
-    print("Costruire il saluto")
+    print("Costruire il saluto".rjust(40, u'\u00b7'))
     return f"Ciao {nome}"
 
 
-s_julia = r_selam("Julia")
-print(s_julia)
+s_sy= r_selam("Sener")
+print(s_sy.rjust(18, u'\u2219'))
 
 
+# @timer decorator
 @timer
 def waste_some_time(num_times):
     for _ in range(num_times):
@@ -76,4 +78,113 @@ def waste_some_time(num_times):
 
 
 waste_some_time(1)
-waste_some_time(9999)
+waste_some_time(51)
+
+
+# Historic Person Data
+DH = {
+    'p1': {
+        'name': "Halid ibn al-Walid",
+        'nick': "The Great General",
+        'predecessor': "",
+        'successor': "",
+        'father': "Mughirah",
+        'mother': "",
+        'title': {
+            "Commander-in-Chief",
+            "Military governor of Iraq",
+            "Governor of Chalcis" },
+        'born': 585,
+        'died': 642,
+        'burial': "Khalid ibn al-Walid mosque, Homs, Syria",
+        'allegiance': "Rashidun Caliphate",
+        'service': "Rashidun Islamic Army",
+        'service_year': "632-638",
+        'rank': "Commander-in-chief",
+        'ref': "https://en.wikipedia.org/wiki/Khalid_ibn_al-Walid",
+    },
+    'p2': {
+        'name': "Sultan Mehmed Han bin Murad Han",
+        'nick': "The Conqueror",
+        'predecessor': "Murad 2",
+        'successor': "Bayezid 2",
+        'father': "Murad 2",
+        'mother': "Huma Hatun",
+        'title': {
+            "Sultan of the Ottoman Empire",
+            "Kayser-i Rûm",
+            "The Lord of the Two Lands and the Two Seas",
+            "Padishah" },
+        'born': 1432,
+        'died': 1481,
+        'burial': "Fatih Mosque, Istanbul",
+        'allegiance': "Ottoman Dynasty",
+        'service': "Ottoman Empire",
+        'service_year': "1451-1481",
+        'rank': "Emperor / Padishah"
+    },
+    'p3': {
+        'name': "Alp Arslan",
+        'nick': "Sultan of the Seljuk Empire",
+        'predecessor': "Tughril",
+        'successor': "Malik-Shah 1",
+        'father': "Chaghri Bey",
+        'mother': "",
+        'title': {
+            "Sultan Alp Arslan",
+            "Seljuk Emperor",
+        },
+        'born': 1030,
+        'died': 1072,
+        'burial': "",
+        'allegiance': "House of Seljuk",
+        'service': "Seljuk Empire",
+        'service_year': "1063-1072",
+        'rank': "Sultan"
+    },
+    'p4': {
+        'name': "Abu Ali Hasan ibn Ali Tusi",
+        'nick': "Nizam al-Mulk",
+        'predecessor': "Al-Kunduri",
+        'successor': "Taj al-Mulk Abu'l Ghana'im",
+        'father': "Ali Tusi",
+        'mother': "",
+        'title': {
+            "Vizier of the Seljuk Empire",
+        },
+        'born': 1018,
+        'died': 1092,
+        'burial': "",
+        'allegiance': "Seljuk Empire",
+        'service': "Seljuk Empire",
+        'service_year': "1064-1092",
+        'rank': "Grand Vizier"
+    },
+
+}
+
+
+# @debug
+@debug
+def make_greeting(*name, age=None):
+    if age is None:
+        return f"As-salam {name}! Unknown age.."
+    else:
+        return f"As-salamu-alaykum {name}; {age}!"
+
+
+make_greeting(DH['p1']['name'], DH['p1']['nick'],
+              DH['p1']['father'],
+              age=DH['p1']['died'] - DH['p1']['born'])
+
+make_greeting(DH['p2']['name'], DH['p2']['nick'],
+              DH['p2']['father'],
+              age=DH['p2']['died'] - DH['p2']['born'])
+
+make_greeting(DH['p3']['name'], DH['p3']['nick'],
+              DH['p3']['father'],
+              age=DH['p3']['died'] - DH['p3']['born'])
+
+make_greeting(DH['p4']['name'], DH['p4']['nick'],
+              DH['p4']['father'],
+              age=DH['p4']['died'] - DH['p4']['born'])
--- decorlib.py
+++ decorlib.py
@@ -10,6 +10,7 @@ import time
 
 
 def decorator(func):
+    ''' decorator func '''
     @functools.wraps(func)
     def wrapper_decorator(*args, **kwargs):
         value = func(*args, **kwargs)
@@ -29,3 +30,16 @@ def timer(func):
         return value
     return wrapper_timer
 
+
+def debug(func):
+    ''' print the function signature and return value '''
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")
+        return value
+    return wrapper_debug
