77fc93bac4f478090478b628366fd031f6e780d3
CodeRonin S <45330933+CodeRoninSY@users.noreply.github.com>
2019-04-21 21:42:34 +0300

    Add files via upload

--- hero_class_v2.py
+++ hero_class_v2.py
@@ -122,59 +122,7 @@ class Hero():
         self.losses += loss
 
 
-## Hero class default
-hero0 = Hero()
-
-print(f"Hero : {hero0!s}")
-print(f"Hero name: {hero0.name}")
-print(f"Hero items: {hero0.items}")
-print(f"Hero stat: {hero0.healthstat}")
-print(f"Hero wins: {hero0.wins}")
-
-# generate a Hero object
-hero = Hero(name='Thor', ID=1, health=85, level=2,
-            inventory=['Mjolnir', 'Chariot', 'Bilskirnir'], wins=2, kills=10,
-            clan="Asgardian", country='Asgard',
-            archenemy=['Thanos', 'Hella', 'Malekith'])
-
-print(f"Hero name: {hero.name}")
-print(f"Hero items: {hero.items}")
-print(f"Hero stat: {hero.healthstat}")
-
-hero.additem('Bifrost')
-Hero.set_level(hero, 5)
-print(f"Hero {hero.name} items: {hero.items}")
-print(f"Hero {hero.name} level: {hero.level}")
-print(f"Hero {hero.name} wins: {hero.wins}")
-
-hero.dropItem('Bifrost')
-hero.addWin(4)
-print(f"Hero {hero.name}, items: {hero.items}")
-print(f"Hero {hero.name} wins: {hero.wins}")
-print(f"Hero {hero.name}, kill #: {hero.kills}")
-print(f"Hero {hero.name}, kill total #: {hero.kills}")
-hero.kill = 12
-print(f"Hero {hero.name}, kill #: {hero.kills}")
-hero.addKill(5)
-print(f"Hero {hero.name}, kill #: {hero.kills}")
-print(f"Hero {hero.name}, health: {hero.health}")
-hero.incHealth(10)
-print(f"Hero {hero.name}, health: {hero.health}")
-hero.incHealth(10)
-print(f"Hero {hero.name}, health: {hero.health}")
-print(f"Hero {hero.name}, alive: {hero.alive}")
-
-hero.addLoss(2)
-print(f"Hero {hero.name}, losses: {hero.losses}")
-
-# full object
-print(f"Hero {hero!s}")
-print(f"Hero {hero!r}")
-
-print(f"Number of heros {Hero.numHeros}")
-
-
-# json data
+# read json file
 def ReadJSONData(filename, raw=False):
     try:
         data = json.load(open(filename))
@@ -185,24 +133,84 @@ def ReadJSONData(filename, raw=False):
             return data["heros"]
         return [Hero(**hero) for hero in data["heros"]]
 
-#  HEROS = ReadJSONData("heros.json")
-R_HEROS = ReadJSONData("heros.json", raw=True)
 
-#  pprint(HEROS)
-#  pprint(R_HEROS)
+def main():
+    ## Hero class default
+    hero0 = Hero()
 
-#  ids = sorted(R_HEROS, key=itemgetter('ID'))
-#  print(f"Sorted IDs[0]: {ids[0]['ID']}, {ids[0]['name']}, {ids[0]['archenemy']}")
+    print(f"Hero : {hero0!s}")
+    print(f"Hero name: {hero0.name}")
+    print(f"Hero items: {hero0.items}")
+    print(f"Hero stat: {hero0.healthstat}")
+    print(f"Hero wins: {hero0.wins}")
 
-for i, h in enumerate(R_HEROS):
-    eroe = Hero(**h)
-    print(f"\nHero name: {eroe.name}, kills: {eroe.kills}, health: {eroe.health},\
-          inventory: {eroe.inventory}")
-    eroe.addKill(8)
-    eroe.additem('Katana')
-    print(f"\nHero name: {eroe.name}, kills: {eroe.kills}, health: {eroe.health},\
-          inventory: {eroe.inventory}")
-    eroe.deleteItemN(-1)
-    print(f"\nHero name: {eroe.name}, kills: {eroe.kills}, health: {eroe.health},\
-          inventory: {eroe.inventory}")
-    print(f"\nTotal generated heros: {Hero.numHeros}")
+    # generate a Hero object
+    hero = Hero(name='Thor', ID=1, health=85, level=2,
+                inventory=['Mjolnir', 'Chariot', 'Bilskirnir'], wins=2, kills=10,
+                clan="Asgardian", country='Asgard',
+                archenemy=['Thanos', 'Hella', 'Malekith'])
+
+    print(f"Hero name: {hero.name}")
+    print(f"Hero items: {hero.items}")
+    print(f"Hero stat: {hero.healthstat}")
+
+    hero.additem('Bifrost')
+    Hero.set_level(hero, 5)
+    print(f"Hero {hero.name} items: {hero.items}")
+    print(f"Hero {hero.name} level: {hero.level}")
+    print(f"Hero {hero.name} wins: {hero.wins}")
+
+    hero.dropItem('Bifrost')
+    hero.addWin(4)
+    print(f"Hero {hero.name}, items: {hero.items}")
+    print(f"Hero {hero.name} wins: {hero.wins}")
+    print(f"Hero {hero.name}, kill #: {hero.kills}")
+    print(f"Hero {hero.name}, kill total #: {hero.kills}")
+    hero.kill = 12
+    print(f"Hero {hero.name}, kill #: {hero.kills}")
+    hero.addKill(5)
+    print(f"Hero {hero.name}, kill #: {hero.kills}")
+    print(f"Hero {hero.name}, health: {hero.health}")
+    hero.incHealth(10)
+    print(f"Hero {hero.name}, health: {hero.health}")
+    hero.incHealth(10)
+    print(f"Hero {hero.name}, health: {hero.health}")
+    print(f"Hero {hero.name}, alive: {hero.alive}")
+
+    hero.addLoss(2)
+    print(f"Hero {hero.name}, losses: {hero.losses}")
+
+    # full object
+    print(f"Hero {hero!s}")
+    print(f"Hero {hero!r}")
+
+    print(f"Number of heros {Hero.numHeros}")
+
+
+    #  HEROS = ReadJSONData("heros.json")
+    R_HEROS = ReadJSONData("heros.json", raw=True)
+
+    #  pprint(HEROS)
+    #  pprint(R_HEROS)
+
+    #  ids = sorted(R_HEROS, key=itemgetter('ID'))
+    #  print(f"Sorted IDs[0]: {ids[0]['ID']}, {ids[0]['name']}, {ids[0]['archenemy']}")
+
+    for i, h in enumerate(R_HEROS):
+        eroe = Hero(**h)
+        print(f"\nHero name: {eroe.name}, kills: {eroe.kills}, health: {eroe.health},\
+            inventory: {eroe.inventory}")
+        eroe.addKill(8)
+        eroe.additem('Katana')
+        print(f"\nHero name: {eroe.name}, kills: {eroe.kills}, health: {eroe.health},\
+            inventory: {eroe.inventory}")
+        eroe.deleteItemN(-1)
+        print(f"\nHero name: {eroe.name}, kills: {eroe.kills}, health: {eroe.health},\
+            inventory: {eroe.inventory}")
+        print(f"\nTotal generated heros: {Hero.numHeros}")
+
+
+# MAIN
+if __name__ == "__main__":
+    main()
+
