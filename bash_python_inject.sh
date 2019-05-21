a449205dc50bff6451caa773ce52d9cb7dbf4be5
CodeRonin S <45330933+CodeRoninSY@users.noreply.github.com>
2019-05-22 00:11:05 +0300

    Add files via upload

---
+++ bash_python_inject.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function current_datetime {
+python - <<END
+import datetime
+print(datetime.datetime.now())
+END
+}
+
+current_datetime
+
+DT=$(current_datetime)
+echo "Current date and time: $DT"
