87b301d5997eb04998858bfb5deb1f506b0d4fe3
CodeRonin S <45330933+CodeRoninSY@users.noreply.github.com>
2019-05-23 00:28:07 +0300

    Add files via upload

--- bash_python_inject_v2.sh
+++ bash_python_inject_v2.sh
@@ -8,8 +8,35 @@ print('-' * line_len)
 END
 }
 
+function to_upper() {
+P1="$1" P2="$2" python - <<EOF
+import os
+p1 = os.environ['P1'].upper()
+p2 = os.environ['P2'].upper()
+print(f"{p1} {p2}")
+EOF
+}
+
+to_upper lensman kim
+
+st=$(date +%s.%N)
+
 line 80
 
 echo "Handy"
 
 echo $(line 80)
+
+sleep 3
+end=$(date +%s.%N)
+ds=$(echo "$end - $st" | bc -l)
+
+# var
+awk -v d="$ds" 'BEGIN {print "delta_s: " d/60.0 " min."}'
+# here-doc
+awk '{print "delta_ds: " $0 " sec."}' <<< "$ds"
+# ARGV
+awk 'BEGIN {print ARGV[1]}' "$ds"
+# ENVIRON
+export ds
+awk 'BEGIN{print ENVIRON["ds"]}'
