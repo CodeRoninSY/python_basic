e48f052fb6f692984ba145bc2dc3589dc8b72576
CodeRonin S <45330933+CodeRoninSY@users.noreply.github.com>
2019-05-26 23:13:13 +0300

    Add files via upload

--- subprocess_example.py
+++ subprocess_example.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-# -*- coding: windows-1252 -*-
+# -*- coding: iso-8859-9 -*-
 '''
-
 subprocess_example.py
 
 Author: CodeRoninSY
 Date: <2019-05-19>
-
 '''
 
 import shlex
@@ -15,11 +13,12 @@ import click
 
 
 cmd_line = input('Enter command >> ')
+click.secho("cmd_line >> '%s'" % cmd_line, fg='green', bold=True)
 args = shlex.split(cmd_line)
 click.secho("args >> %s" % args, fg='green', bold=True)
-p = subprocess.Popen(args)
+p = subprocess.Popen(cmd_line, shell=True)
 
 poll = subprocess.Popen.poll(p)
 click.secho("poll >> %s" % poll, fg='yellow')
 click.secho("pid >> %s" % p.pid, fg='white', bg='blue', bold=True)
-print(p.returncode)
+click.secho("returncode >> %s" % p.returncode, fg='white', bg='blue', bold=True)
